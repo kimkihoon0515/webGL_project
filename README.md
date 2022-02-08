5f73d6fe0436a186df6b72e914f13edb4289649e
kihoon kim <tega1996@naver.com>
2022-02-08 14:12:01 +0900

    컴퓨터 그래픽스 기말프로젝트

---
+++ README.md
@@ -0,0 +1,97 @@
+# 2021년 1학기 컴퓨터 그래픽스 WebGL Tutorial Page
+## Cube에 원하는 이미지 Texture 입히고 마우스 event 추가하기
+이 페이지는 큐브에 Texture를 입히고 그 크기를 조절해보고 마우스를 통해서 원하는 면을 관찰해 보는 튜토리얼 페이지이다.
+
+## 이름
+소프트웨어학과 201620949 김기훈
+
+## 주의사항
+Texture를 입히는 과정에서 이미지 파일을 로컬에서 크롬브라우저가 보안상의 이유로 불러올 수 없기 때문에 Web Server For Chrome을 사용한다.
+![Web](/uploads/b030e33d19e8fc03767c92dedf97ad0d/Web.png)
+
+## Texture Mapping 을 하는 이유
+![texture](/uploads/4676c16b4ca19f39c06f8336426e9820/texture.png)  
+사실적인 그림을 낮은 비용으로 만들 수 있기 때문 
+
+## Texture Mapping 을 하기 위해서 해야하는 것
+### 1. 가장 먼저 텍스쳐를 불러와야한다.    
+![texturemap](/uploads/97d0c8069e2143df6dcb30a2de60e312/texturemap.png)  
+이번 프로젝트에서는 하나의 이미지 텍스쳐를 모든 면에 Mapping 해보겠습니다.   
+### 2. 크기가 2의 거듭제곱인지 확인한다.    
+![texturemap](/uploads/7fc98dad69c4ef32d017ba4498078276/texturemap.png)
+![texturemap](/uploads/866db6fd53dbf239ac1aeebc3419a578/texturemap.png)  
+일반적으로 너비와 높이가 2의 거듭제곱인 텍스쳐를 사용하는 것이 가장 이상적입니다. 왜냐하면 2의 거듭제곱인 텍스쳐는 비디오 메모리에 효율적으로 저장될 수 있고, 어떤 방식으로 사용되어야만 한다는 제약이 없기 때문입니다.  
+예술가들이 이미 작성한 텍스쳐는 너비와 높이가 2의 거듭제곱이 되도록 크기를 맞춰줘야 하며, 가능하다면 아예 만들때부터 2의 거듭제곱으로 만드는 것이 좋습니다.  
+너비와 높이는 2의 거듭제곱인 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 또는 2048 픽셀이어야 합니다.  
+전부는 아니지만 많은 디바이스가 4096 픽셀도 지원하고 있으며, 어떤 디바이스는 8192 픽셀 이상을 지원하기도 합니다.      
+### 3. 면에 텍스쳐 입히기  
+![texturemap](/uploads/30a2fbf9f661e59080c748b4643d5330/texturemap.png)  
+텍스쳐를 사용하기 전에 텍스쳐의 좌표와 정육면체의 면의 정점을 매핑 시켜줘야 합니다.    
+### 4. fragment shader 수정  
+![texturemap](/uploads/c384b4eb5b9d98d667680c03e03376ab/texturemap.png)
+이렇게 하면 프래그먼트의 색상을 정하기 위해 직접 프래그먼트에 색상값을 할당하지 않고, 샘플러(sampler)가 판단하기에 프래그먼트의 위치에 가장 잘 맞아 떨어진다고 여겨지는 텍셀(texel, 텍스쳐 내부에 있는 픽셀)값에 따라서 프래그먼트의 색상값을 계산해냅니다.  
+### 5. vertex shader 수정  
+![texturemap](/uploads/59d0f49d08d66e26b4159aafb1779294/texturemap.png)  
+정점 색상 정보를 읽어오는 대신에 텍스쳐 좌표값을 읽어와서 설정한다는 점이 키 포인트 입니다.  
+위와 같이 정점과 텍스쳐 좌표값을 매핑하면, 각 정점이 텍스쳐의 어느 지점에 해당 하는지 알려줄 수 있습니다.     
+### 6. 사진 변경사항을 적용하는 코드 추가  
+![texturemap](/uploads/5759a8e05d186e36eec3538bbcc6a416/texturemap.png)  
+File API는 사용자에 의해 선택된 파일을 나타내는 객체인 File을 포함하는 FileList에 접근할 수 있게 해줍니다.  
+사용자가 하나의 파일만을 선택한 경우, 리스트의 첫 번째 파일만 고려하면 됩니다.  
+그 후 불러온 이미지 너비와 높이가 2의 거듭제곱인지 확인하는 과정을 거친다.  
+## 구현 화면
+![picture](/uploads/53435517cb774194db9037c8f0b4e9a1/picture.png)  
+원하는 사진을 data 폴더 내에서 골라준다. 기본 이미지는 고양이 이미지이다.  
+![c](/uploads/92aa340aa38e9c59fd7bce6822aeb4d6/c.png)  
+아주대 로고를 선택하고 변경을 누르면 다음과 같이 내가 선택한 사진으로 큐브의 이미지가 변경이 된다.
+
+## Mouse Control event
+### 큐브를 마우스 드래그와 휠 조절로 크기를 조절하고 보이는 면을 조절하는 기능이다.  
+
+### 1. Mouse Click event  
+이벤트 핸들러는 하나의 개체 (이벤트 개체)를 매개 변수로받는 함수입니다.  
+다양한 유형의 이벤트는 다양한 유형의 이벤트 객체를 생성합니다.  
+.target : The DOM element that initiated the event.  
+.pageX : The mouse position relative to the left edge of the document.  
+.pageY : The mouse position relative to the top edge of the document.  
+.clientX : The mouse position relative to the left edge of the element.  
+.clientY : The mouse position relative to the top edge of the element.  
+.which : The specific key or button that was pressed.  
+마우스 클릭을 해서 드래그한 방향으로 큐브가 돌아가게끔 추가해주는 코드이다.   
+마우스의 이전 위치에서 오프셋을 계산하고 해당 오프셋을 사용하여 장면의 회전 각도를 수정합니다.  
+단일 이벤트 핸들러에서 이벤트를 처리하려면 이벤트 객체에 대해 preventDefault ()를 호출하여 해당 이벤트가 상위 요소로 전달되지 않도록해야합니다.        
+![texturemap](/uploads/7660e2160a849dbc9a8c640493ecbf0d/texturemap.png)    
+### 2. Mouse Wheel event  
+마우스 클릭과 마찬가지의 방법으로 이번에는 마우스 가운데에 있는 wheel을 이용해서 큐브를 확대 또는 축소하는 기능을 갖게끔 해주는 코드이다.    
+![texturemap](/uploads/659c2e0b57acbf355f80cd7890fd466b/texturemap.png)
+### 3. Add EventListener
+앞에서 구현한 Mouse event 관련 기능들을 전부 canvas 내에서 EventListener로 호출해준다.  
+mouseout : 마우스가 element를 벗어났을 때  
+mousemove : 마우스가 element에서 움직일 때
+mousedown : 마우스를 누르는 순간  
+mouse up : down과 반대로 마우스를 누르고 있다가 손가락을 떼는 그 순간  
+wheel : 마우스 wheel을 누르는 순간  
+![texturemap](/uploads/b57afcd41b6e12139fb6a621bf083a4e/texturemap.png)
+
+## 구현 화면
+![mouse](/uploads/9ca80b25d1099ba08d83560c4d7dff33/mouse.gif)
+
+
+## Review
+간단해보였던 이미지 mapping이 이렇게나 까다로운 과정일줄 정말 몰랐었다. 이미지 좌표와 큐브좌표를 딱 맞춰서 대입해야해서 까다로웠고 초기에 기획했었던 큐브로 주사위를 만들기는 아쉽지만 개별적으로 진행해보기로 하였다. 특히 이미지를 불러오는데 사이즈가 달라서 이미지가 적용이 안되던 문제도 많았던 것 같다. 개인적으로 정말 흥미롭게 들었던 수업이니만큼 꼭 개별적으로 시간을 내서 이번 프로젝트에선 하지못했지만 초기에 계획했던 것들을 한번 해보면서 공부해보려 한다. 
+
+## 참조 및 출처
+https://developer.mozilla.org/ko/docs/Web/API/WebGL_API/Tutorial/Using_textures_in_WebGL
+http://learnwebgl.brown37.net/browser_environment/events.html
+https://git.ajou.ac.kr/hwan/webgl-tutorial/-/tree/master/student2020/better_project/201420989
+https://git.ajou.ac.kr/hwan/webgl-tutorial/-/tree/master/student2020/better_project/201720579  
+Based on Project https://github.com/hwan-ajou/webgl-1.0/tree/main/T08_culling 
+
+
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,398 @@
+var gl;
+
+const {mat2, mat3, mat4, vec2, vec3, vec4} = glMatrix;  // Now we can use function without glMatrix.~~~
+function testGLError(functionLastCalled) {
+    /* gl.getError returns the last error that occurred using WebGL for debugging */ 
+    var lastError = gl.getError();
+
+    if (lastError != gl.NO_ERROR) {
+        alert(functionLastCalled + " failed (" + lastError + ")");
+        return false;
+    }
+    return true;
+}
+
+function initialiseGL(canvas) {
+    try {
+        // Try to grab the standard context. If it fails, fallback to experimental
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        gl.viewport(0, 0, canvas.width, canvas.height);
+    }
+    catch (e) {
+    }
+
+    var drag = false;
+    var old_x, old_y;
+
+    var Down = function(e) {
+
+        drag = true;
+        old_x = e.pageX;
+        old_y = e.pageY;
+        e.preventDefault();
+        return false;
+    };
+
+    var Up = function(e) {
+        drag = false;
+    };
+
+    var Move = function(e) {
+        if (!drag) {
+            return false;
+        }
+        var dX = e.pageX - old_x;
+        var dY = e.pageY - old_y;
+
+        yRot += dX * 3 / canvas.width;
+        xRot += dY*3 / canvas.height;
+
+        old_x = e.pageX;
+        old_y = e.pageY;
+
+        e.preventDefault();
+    };
+
+    var wheel = function(e) {
+        if(e.deltaY > 0) {
+            if(scale[0]> 0.3){
+                for(i=0;i<3;i++){
+                    scale[i] -= 0.2;
+                }
+            }
+        }
+        else {
+            if(scale[0] < 1.5){
+                for(i=0;i<3;i++){
+                    scale[i] += 0.2;
+                }
+            }
+        }
+        e.preventDefault();
+    };
+
+    canvas.addEventListener("mousedown",Down, false);
+    canvas.addEventListener("mouseup", Up, false);
+    canvas.addEventListener("mouseout", Up, false);
+    canvas.addEventListener("mousemove", Move, false);
+    canvas.addEventListener("wheel", wheel, false);
+
+    if (!gl) {
+        alert("Unable to initialise WebGL. Your browser may not support it");
+        return false;
+    }
+    return true;
+}
+
+
+var shaderProgram;
+
+function makeCube(sx, sy, sz){
+    var x = sx/2;
+    var y = sy/2;
+    var z = sz/2;
+    var vertexData = [
+         //Back
+          -x, -y, -z,  1.0, 0.0, 0.0, 1.0,  0.0,  0.0,
+         x,  y, -z,  1.0, 0.0, 0.0, 1.0,  1.0,  1.0,
+         x, -y, -z,  1.0, 0.0, 0.0, 1.0,  1.0,  0.0,
+         -x, -y, -z,  1.0, 0.0, 0.0, 1.0,  0.0,  0.0,
+          -x,  y, -z,  1.0, 0.0, 0.0, 1.0,  0.0,  1.0,
+          x,  y, -z,  1.0, 0.0, 0.0, 1.0,  1.0,  1.0,
+      // Front (BLUE/WHITE) -> z = 0.5           
+          -x, -y,  z,  0.0, 0.0, 1.0, 1.0,  0.0,  0.0,
+           x, -y,  z,  0.0, 0.0, 1.0, 1.0,  1.0,  0.0,
+          x,  y,  z,  0.0, 0.0, 1.0, 1.0,  1.0,  1.0,
+         -x, -y,  z,  0.0, 0.0, 1.0, 1.0,  0.0,  0.0,
+          x,  y,  z,  0.0, 0.0, 1.0, 1.0,  1.0,  1.0, 
+         -x,  y,  z,  0.0, 0.0, 1.0, 1.0,  0.0,  1.0,
+      // LEFT (GREEN/WHITE) -> z = 0.5                       
+         -x, -y, -z,  0.0, 1.0, 0.0, 1.0,  0.0,  0.0,
+         -x,  y,  z,  0.0, 1.0, 0.0, 1.0,  1.0,  1.0,
+         -x,  y, -z,  0.0, 1.0, 0.0, 1.0,  1.0,  0.0,
+         -x, -y, -z,  0.0, 1.0, 0.0, 1.0,  0.0,  0.0,
+         -x, -y,  z,  0.0, 1.0, 0.0, 1.0,  0.0,  1.0,
+        -x,  y,  z,  0.0, 1.0, 0.0, 1.0,  1.0,  1.0, 
+      // RIGHT (YELLOW/WHITE) -> z = 0.5         
+          x, -y, -z,  1.0, 1.0, 0.0, 1.0,  0.0,  0.0,
+         x,  y, -z,  1.0, 1.0, 0.0, 1.0,  1.0,  0.0,
+        x,  y,  z,  1.0, 1.0, 0.0, 1.0,  1.0,  1.0,
+        x, -y, -z,  1.0, 1.0, 0.0, 1.0,  0.0,  0.0,
+         x,  y,  z,  1.0, 1.0, 0.0, 1.0,  1.0,  1.0, 
+         x, -y,  z,  1.0, 1.0, 0.0, 1.0,  0.0,  1.0,
+      // BOTTON (MAGENTA/WHITE) -> z = 0.5                   
+        -x, -y, -z,  1.0, 0.0, 1.0, 1.0,  0.0,  0.0,
+        x, -y, -z,  1.0, 0.0, 1.0, 1.0,  1.0,  0.0,
+        x, -y,  z,  1.0, 0.0, 1.0, 1.0,  1.0,  1.0,
+       -x, -y, -z,  1.0, 0.0, 1.0, 1.0,  0.0,  0.0,
+        x, -y,  z,  1.0, 0.0, 1.0, 1.0,  1.0,  1.0, 
+       -x, -y,  z,  1.0, 0.0, 1.0, 1.0,  0.0,  1.0,
+        // TOP (CYAN/WHITE) -> z = 0.5                         
+        -x,  y, -z,  0.0, 1.0, 1.0, 1.0,  0.0,  0.0,
+         x,  y,  z,  0.0, 1.0, 1.0, 1.0,  1.0,  1.0,
+        x,  y, -z,  0.0, 1.0, 1.0, 1.0,  1.0,  0.0,
+       -x,  y, -z,  0.0, 1.0, 1.0, 1.0,  0.0,  0.0,
+       -x,  y,  z,  0.0, 1.0, 1.0, 1.0,  0.0,  1.0,
+        x,  y,  z,  0.0, 1.0, 1.0, 1.0,  1.0,  1.0
+    ];
+    return vertexData;
+}
+
+
+
+
+var texture
+var image;
+
+
+function initialiseBuffer() {
+
+    gl.vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer);
+    vertexData = makeCube(1.0, 1.0, 1.0);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+    texture = gl.createTexture();
+    image = new Image();
+    image.src = "./data/cat.jpg";
+    image.addEventListener('load', function () {
+        // Now that the image has loaded make copy it to the texture.
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+            // Yes, it's a power of 2. Generate mips.
+            gl.generateMipmap(gl.TEXTURE_2D);
+         } else {
+            // No, it's not a power of 2. Turn of mips and set wrapping to clamp to edge
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+         }
+    });
+
+    const selectImage = document.getElementById('userImage');
+
+    selectImage.addEventListener('change', function() {
+        image.src ="./data/"+ document.getElementById('userImage').files[0].name;
+        image.onload = function() {
+            if(!isPowerOf2(image.width)||!isPowerOf2(image.height)) {
+                alert('Your image size is ' + this.width + ' × ' + this.height + ' pixels.' +
+                '\n\nOpenGL ES 2.0 and WebGL have only limited NPOT support.' +
+                '\n\nPlease upload an image that meets the size of 128 x 128.');
+                location.reload(true);
+            }
+        }
+    });
+
+
+    return testGLError("initialiseBuffers");
+}
+
+
+
+function initialiseShaders() {
+
+    var fragmentShaderSource = `
+			varying highp vec4 col; 
+            varying highp vec2 uv;
+            uniform sampler2D texture;
+			void main(void) 
+			{ 
+				gl_FragColor = 0.0 * col + 1.0 * texture2D(texture, uv);
+			}`;
+
+    gl.fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(gl.fragShader, fragmentShaderSource);
+    gl.compileShader(gl.fragShader);
+    // Check if compilation succeeded
+    if (!gl.getShaderParameter(gl.fragShader, gl.COMPILE_STATUS)) {
+        alert("Failed to compile the fragment shader.\n" + gl.getShaderInfoLog(gl.fragShader));
+        return false;
+    }
+
+    // Vertex shader code
+    var vertexShaderSource = `
+			attribute highp vec4 myVertex; 
+			attribute highp vec4 myColor;
+            attribute highp vec2 myUV; 
+			uniform mediump mat4 mMat; 
+			uniform mediump mat4 vMat; 
+			uniform mediump mat4 pMat; 
+			varying  highp vec4 col;
+            varying mediump vec2 uv;
+			void main(void)  
+			{ 
+				gl_Position = pMat * vMat * mMat * myVertex; 
+				gl_PointSize = 8.0;
+				col = myColor; 
+                uv = myUV;
+			}`;
+
+    gl.vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(gl.vertexShader, vertexShaderSource);
+    gl.compileShader(gl.vertexShader);
+    // Check if compilation succeeded
+    if (!gl.getShaderParameter(gl.vertexShader, gl.COMPILE_STATUS)) {
+        alert("Failed to compile the vertex shader.\n" + gl.getShaderInfoLog(gl.vertexShader));
+        return false;
+    }
+
+    // Create the shader program
+    gl.programObject = gl.createProgram();
+    // Attach the fragment and vertex shaders to it
+    gl.attachShader(gl.programObject, gl.fragShader);
+    gl.attachShader(gl.programObject, gl.vertexShader);
+    // Bind the custom vertex attribute "myVertex" to location 0
+    gl.bindAttribLocation(gl.programObject, 0, "myVertex");
+    gl.bindAttribLocation(gl.programObject, 1, "myColor");
+    gl.bindAttribLocation(gl.programObject, 2, "myUV");
+    // Link the program
+    gl.linkProgram(gl.programObject);
+    // Check if linking succeeded in a similar way we checked for compilation errors
+    if (!gl.getProgramParameter(gl.programObject, gl.LINK_STATUS)) {
+        alert("Failed to link the program.\n" + gl.getProgramInfoLog(gl.programObject));
+        return false;
+    }
+
+    gl.useProgram(gl.programObject);
+
+    return testGLError("initialiseShaders");
+}
+
+function isPowerOf2(value) {
+    return (value & (value - 1)) === 0;
+  }
+
+
+var xRot = 0.0;
+var yRot = 0.0;
+var zRot = 0.0;
+var speedRot = 0.01; 
+var scale = [1.0, 1.0, 1.0];
+
+flag_animation = 0; 
+function toggleAnimation()
+{
+	flag_animation ^= 1; 
+	console.log("flag_animation=", flag_animation);
+}
+
+function speed_scale(a)
+{
+	speedRot *= a; 
+}
+
+var draw_mode = 4; // 4 Triangles, 3 line_strip 0-Points
+
+function fn_draw_mode(a)
+{
+	draw_mode = a;
+}
+
+var fov_degree = 90.0; 
+function fn_update_fov(val)
+{
+	document.getElementById('textFOV').value=val; 
+	fov_degree = val; 
+}
+
+var mMat, vMat, pMat; 
+
+function renderScene() {
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clearDepth(1);										// Added for depth Test 
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);	// Added for depth Test 
+	gl.enable(gl.DEPTH_TEST);								// Added for depth Test 
+
+    var mMatLocation = gl.getUniformLocation(gl.programObject, "mMat");
+	var vMatLocation = gl.getUniformLocation(gl.programObject, "vMat");
+	var pMatLocation = gl.getUniformLocation(gl.programObject, "pMat");
+    pMat = mat4.create(); 
+	vMat = mat4.create(); 
+	mMat = mat4.create(); 
+	// mat4.ortho(pMat, -8.0/6.0, 8.0/6.0, -1, 1, -1, 1); 
+	// mat4.frustum(pMat, -8.0/6.0, 8.0/6.0, -1, 1, 0.5, 3); 
+	mat4.perspective(pMat, fov_degree * 3.141592 / 180.0 , 8.0/6.0 , 0.5, 8); 
+	mat4.lookAt(vMat, [0,0,2], [0.0,0.0,0.0], [0,1,0]);
+	// console.log(pMat);
+	// mat4.translate(mMat, mMat, [0,0,-1]);
+	mat4.rotateX(mMat, mMat, xRot);
+	mat4.rotateY(mMat, mMat, yRot);
+	mat4.rotateZ(mMat, mMat, zRot);
+    mat4.scale(vMat, vMat, scale);
+	
+    var textureLocation = gl.getUniformLocation(gl.programObject, "texture");
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.uniform1i(textureLocation,0);
+	
+	if (flag_animation == 1)
+	{
+		xRot = xRot + speedRot;	
+		yRot = yRot + speedRot;	
+		// zRot = zRot + speedRot;	
+    }
+	
+	gl.uniformMatrix4fv(mMatLocation, gl.FALSE, mMat );
+	gl.uniformMatrix4fv(vMatLocation, gl.FALSE, vMat );
+	gl.uniformMatrix4fv(pMatLocation, gl.FALSE, pMat );
+
+    if (!testGLError("gl.uniformMatrix4fv")) {
+        return false;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.vertexBuffer);
+    gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 36, 0);
+    gl.enableVertexAttribArray(1);
+    gl.vertexAttribPointer(1, 4, gl.FLOAT, gl.FALSE, 36, 12);
+    gl.enableVertexAttribArray(2);
+    gl.vertexAttribPointer(2, 2, gl.FLOAT, gl.FALSE, 36, 28);
+
+    if (!testGLError("gl.vertexAttribPointer")) {
+        return false;
+    }
+
+	gl.drawArrays(draw_mode, 0, 36); 
+	var saveMat = mat4.create();
+
+    if (!testGLError("gl.drawArrays")) {
+        return false;
+    }
+
+    return true;
+}
+
+function main() {
+    var canvas = document.getElementById("helloapicanvas");
+
+    if (!initialiseGL(canvas)) {
+        return;
+    }
+
+    if (!initialiseBuffer()) {
+        return;
+    }
+
+    if (!initialiseShaders()) {
+        return;
+    }
+
+	// renderScene();
+    // Render loop
+    requestAnimFrame = (function () {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
+			function (callback) {
+			    window.setTimeout(callback, 1000, 60);
+			};
+    })();
+
+    (function renderLoop() {
+        if (renderScene()) {
+            // Everything was successful, request that we redraw our scene again in the future
+            requestAnimFrame(renderLoop);
+        }
+    })();
+}
